373b5b8c5065fdeb19102e6c5ee245e41f601755
Borja Álvarez Piñeiro <borjaalvarez_18@hotmail.com>
2024-09-22 22:35:16 +0200

    feat: [CH-02] Add appointments

--- Chapter02/Start/src/Appointment.js
+++ Chapter02/Start/src/Appointment.js
@@ -1,5 +1,21 @@
 import React from "react";
 
+const appointmentTimeOfDay = (startsAt) => {
+  const [h, m] = new Date(startsAt).toTimeString().split(":");
+  return `${h}:${m}`
+}
+
 export const Appointment = ({ customer }) => (
   <div>{customer.firstName}</div>
 );
+
+export const AppointmentsDayView = (props) => {
+  const { appointments = [] } = props
+  return < div id="appointmentsDayView" >
+    <ol>
+      {appointments.map((appointment) => {
+        return <li key={appointment.startsAt}>{appointmentTimeOfDay(appointment.startsAt)}</li>
+      })}
+    </ol>
+  </div>
+};
--- Chapter02/Start/test/Appointment.test.js
+++ Chapter02/Start/test/Appointment.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { act } from "react-dom/test-utils";
-import { Appointment } from "../src/Appointment";
+import { Appointment, AppointmentsDayView } from "../src/Appointment";
 
 describe("Appointment", () => {
   let container;
@@ -33,4 +33,51 @@ describe("Appointment", () => {
       "Jordan"
     );
   });
+});
+
+describe("AppointmentsDayView", () => {
+  let container;
+  const today = new Date();
+  const twoAppointments = [
+    { startsAt: today.setHours(12, 0) },
+    { startsAt: today.setHours(13, 0) },
+  ];
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+
+  const render = (component) =>
+    act(() =>
+      ReactDOM.createRoot(container).render(component)
+    );
+
+  it("renders a div with the right id", () => {
+
+    render(<AppointmentsDayView />);
+    expect(document.querySelector("div#appointmentsDayView")).not.toBeNull();
+  });
+
+  it("renders an ol element to display appointments", () => {
+    render(<AppointmentsDayView />);
+    const listElement = document.querySelector("ol");
+    expect(listElement).not.toBeNull();
+  });
+
+  it("renders an li for each appointment", () => {
+    render(<AppointmentsDayView appointments={twoAppointments} />);
+    const listElement = document.querySelectorAll("ol > li");
+    expect(listElement).toHaveLength(2);
+  });
+
+  it("renders the time of each appointment", () => {
+    render(<AppointmentsDayView appointments={twoAppointments} />);
+
+    const listChildren = document.querySelectorAll("li");
+    expect(listChildren[0].textContent).toEqual("12:00");
+    expect(listChildren[1].textContent).toEqual("13:00");
+  });
+
+
 });
