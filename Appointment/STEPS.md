b1ebc2e069519dc75c774e127573d23625178929
Borja Álvarez Piñeiro <borjaalvarez_18@hotmail.com>
2024-09-22 11:25:55 +0200

    feat: [CH-01-TS] Initial commit

---
+++ Appointment/STEPS.md
@@ -0,0 +1,11 @@
+# Chapter 01
+npm init
+ press enter to all the entries except the test where you usst write jest
+npm install --save-dev jest
+npm install react react-dom
+npm install --save-dev @babel/preset-env @babel/preset-react
+npm install --save-dev @babel/plugin-transform-runtime
+npm install --save @babel/runtime
+npm install --save-dev jest-environment-jsdom
+
+# Support to ts
---
+++ Appointment/test/Appointment.test.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { act } from 'react';
+
+import { Appointment } from "../src/Appointment";
+
+describe("Appointment", () => {
+    let container;
+
+    const render = (component) => act(() => ReactDOM.createRoot(container).render(component))
+
+    beforeEach(() => {
+        container = document.createElement("div");
+        document.body.replaceChildren(container);
+    });
+
+    it("renders the customer first name", () => {
+        const customer = { firstName: "Ashley" }
+        render(<Appointment customer={customer} />);
+        expect(document.body.textContent).toContain("Ashley");
+    });
+
+    it("renders another customer first name", () => {
+        const customer = { firstName: "Jordan" }
+        render(<Appointment customer={customer} />);
+        expect(document.body.textContent).toContain("Jordan");
+    });
+});
