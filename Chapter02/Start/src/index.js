d4170a6c8927d01ca0ab62eadd3bf0695710cf79
Borja Álvarez Piñeiro <borjaalvarez_18@hotmail.com>
2024-09-23 20:03:31 +0200

    feat: [CH-02] Configure webpack

--- Chapter02/Start/src/index.js
+++ Chapter02/Start/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { AppointmentsDayView } from "./AppointmentsDayView";
+import { AppointmentsDayView } from "./Appointment";
 import { sampleAppointments } from "./sampleData";
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
