9d9c7a77e5bf2361f02007c4683a9c2f8ec697ab
Borja Álvarez Piñeiro <borjaalvarez_18@hotmail.com>
2024-09-28 09:52:41 +0200

    feat: [CH-02-TS] Add appointments

--- Appointment/src/Appointment.tsx
+++ Appointment/src/Appointment.tsx
@@ -9,3 +9,16 @@ interface Props {
 export const Appointment: React.FC<Props> = ({ customer }) => {
     return <div>{customer.firstName}</div>
 };
+
+interface AppointmentsDayViewProps {
+    appointments?: {
+        startsAt: number
+    }[];
+}
+
+export const AppointmentsDayView: React.FC<AppointmentsDayViewProps> = (props) => {
+    const { appointments = [] } = props
+    return <div id="appointmentsDayView">
+        {appointments.map((appointment, index) => <li key={appointment.startsAt}></li>)}
+    </div>
+}
--- Appointment/test/Appointment.test.tsx
+++ Appointment/test/Appointment.test.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 import ReactDOM from "react-dom/client";
 import { act } from 'react';
 
-import { Appointment } from "../src/Appointment";
+import { Appointment, AppointmentsDayView } from "../src/Appointment";
 
 describe("Appointment", () => {
     let container: HTMLDivElement;
@@ -25,4 +25,39 @@ describe("Appointment", () => {
         render(<Appointment customer={customer} />);
         expect(document.body.textContent).toContain("Jordan");
     });
+});
+
+describe("AppointmentsDayView", () => {
+    let container: HTMLDivElement;
+
+    const render = (component: ReactNode) => act(() => ReactDOM.createRoot(container).render(component))
+
+    beforeEach(() => {
+        container = document.createElement("div");
+        document.body.replaceChildren(container);
+    });
+
+    it("renders a div with the right id", () => {
+        render(<AppointmentsDayView />);
+        expect(document.querySelector("div#appointmentsDayView")).not.toBeNull();
+    });
+
+    it("renders an ol element to display appointments", () => {
+        render(<AppointmentsDayView appointments={[]} />);
+        const listElement = document.querySelectorAll("ol")
+        expect(listElement).not.toBeNull();
+    });
+
+    it("renders an li for each appointment", () => {
+        const today = new Date();
+        const twoAppointments = [
+            { startsAt: today.setHours(12, 0) },
+            { startsAt: today.setHours(13, 0) }
+        ];
+        render(<AppointmentsDayView appointments={twoAppointments} />);
+        const listElement = document.querySelectorAll("li")
+        expect(listElement).toHaveLength(2)
+    });
+
+
 });
