d6331670b78ea278896cb32d8af8a7ee12c8ed34
Borja Álvarez Piñeiro <borjaalvarez_18@hotmail.com>
2024-09-28 11:55:39 +0200

    feat: [CH-02-TS] Add webkit

---
+++ Appointment/src/index.tsx
@@ -0,0 +1,12 @@
+import { AppointmentsDayView } from "./Appointment"
+import ReactDOM from "react-dom/client"
+import { sampleAppointments } from "./SampleData";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+    const rootComponent = ReactDOM.createRoot(rootElement);
+    rootComponent.render(<AppointmentsDayView appointments={sampleAppointments} />);
+} else {
+    console.error("Not found element with 'root' id");
+}
