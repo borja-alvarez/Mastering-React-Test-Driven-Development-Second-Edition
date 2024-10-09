fb47f5ee8cace9620fda404c76e868819a5e6322
Borja Álvarez Piñeiro <borjaalvarez_18@hotmail.com>
2024-10-09 21:56:27 +0200

    test: [CH-04] ComponetForm fix form

--- Chapter04/Start/src/CustomerForm.js
+++ Chapter04/Start/src/CustomerForm.js
@@ -3,6 +3,8 @@ export const CustomerForm = ({ original, onSubmit }) => {
   const [customer, setCustomer] = React.useState(original);
 
   const handleChange = (event) => {
+    console.log(event);
+
     setCustomer((customer) => ({
       ...customer,
       [event.target.name]: event.target.value,
@@ -11,17 +13,17 @@ export const CustomerForm = ({ original, onSubmit }) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    onSubmit(customer);
+    onSubmit && onSubmit(customer);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="firstName">First name</label>
-      <input readOnly type="text" id="firstName" name="firstName" value={customer.firstName} onChange={handleChange} />
+      <input type="text" id="firstName" name="firstName" value={customer?.firstName ?? ""} onChange={handleChange} />
       <label htmlFor="lastName">Last name</label>
-      <input readOnly type="text" id="lastName" name="lastName" value={customer.lastName} onChange={handleChange} />
+      <input type="text" id="lastName" name="lastName" value={customer?.lastName ?? ""} onChange={handleChange} />
       <label htmlFor="phoneNumber">Phone number</label>
-      <input readOnly type="text" id="phoneNumber" name="phoneNumber" value={customer.phoneNumber} onChange={handleChange} />
+      <input type="text" id="phoneNumber" name="phoneNumber" value={customer?.phoneNumber ?? ""} onChange={handleChange} />
 
       <input type="submit" value="Add" />
     </form>
--- Chapter04/Start/src/index.js
+++ Chapter04/Start/src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { AppointmentsDayView } from "./AppointmentsDayView";
 import { sampleAppointments } from "./sampleData";
+import { CustomerForm } from "./CustomerForm";
 
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
-  <AppointmentsDayView
-    appointments={sampleAppointments}
-  />
-);
+// ReactDOM.createRoot(
+//   document.getElementById("root")
+// ).render(
+//   <AppointmentsDayView
+//     appointments={sampleAppointments}
+//   />
+// );
+
+ReactDOM.createRoot(document.getElementById("root")).render(<CustomerForm onSubmit={console.log} />);
